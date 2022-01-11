a6128f42183af1222287a3b7957c6194d2ea9e8e
SheebaHashmi <sheebahashmi0@gmail.com>
2022-01-11 15:43:27 -0500

    Create GET /api/posts

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,36 @@
 // implement your posts router here
+const Posts = require('./posts-model');
+const express = require('express');
+const router = express.Router()
+
+router.get('/', async(req,res)=> {
+    try{
+        const post = await Posts.find()
+        res.json(post)
+    }
+    catch{
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    }
+})
+
+// router.get('/:id',(req,res)=> {
+
+// })
+
+// router.post('/',(req,res)=> {
+
+// })
+
+// router.put('/:id',(req,res)=> {
+
+// })
+
+// router.delete('/:id',(req,res)=> {
+
+// })
+
+// router.get('/:id/comments',(req,res)=> {
+
+// })
+
+module.exports = router
